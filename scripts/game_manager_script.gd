cd87d493782db172f35acf566b739df200fe2377
Jakub <jankovec.jakub@gmail.com>
2025-11-29 03:16:12 +0100

    Stuff

--- scripts/game_manager_script.gd
+++ scripts/game_manager_script.gd
@@ -9,20 +9,27 @@ enum GameState {
 	
 	FirstLevelPlay,
 	
+	PreSecondChoice,
 	SecondLevelChoice,
 	PostSecondLevelBeat,
 	AllSecondLevelBeaten,
 	
+	PreThirdLevelChoice,
 	ThirdLevelChoice,
 	ThirdBeat,
 	AllThirdBeaten,
 	
-	Fourth,
+	MidThird,
+	
+	PreFourth,
+	Four,
 	GiveUp,
 	
 	Won,
 	
-	End
+	End,
+	
+	PostEnd
 	}
 
 var nextState := {
@@ -30,27 +37,33 @@ var nextState := {
 	
 	GameState.Introduction : GameState.FirstLevelExplain,
 	GameState.FirstLevelExplain : GameState.FirstLevelPlay,
-	GameState.FirstLevelPlay : GameState.SecondLevelChoice,
+	GameState.FirstLevelPlay : GameState.PreSecondChoice,
 	
+	GameState.PreSecondChoice : GameState.SecondLevelChoice,
 	GameState.SecondLevelChoice : GameState.PostSecondLevelBeat,
-	GameState.PostSecondLevelBeat : GameState.AllSecondLevelBeaten,
-	GameState.AllSecondLevelBeaten : GameState.ThirdLevelChoice,
 	
+	GameState.PostSecondLevelBeat : GameState.AllSecondLevelBeaten,
+	
+	GameState.AllSecondLevelBeaten : GameState.PreThirdLevelChoice,
+	
+	GameState.PreThirdLevelChoice : GameState.ThirdLevelChoice,
 	GameState.ThirdLevelChoice : GameState.ThirdBeat,
 	GameState.ThirdBeat : GameState.AllThirdBeaten,
-	GameState.AllThirdBeaten : GameState.Fourth,
+	GameState.AllThirdBeaten : GameState.PreFourth,
 	
-	GameState.Fourth : GameState.GiveUp,
+	GameState.MidThird : GameState.ThirdLevelChoice,
+	
+	GameState.PreFourth : GameState.Four,
+	GameState.Four : GameState.GiveUp,
+	
 	GameState.GiveUp : GameState.Won,
 	
 	GameState.Won : GameState.End,
 	
-	GameState.End : GameState.Menu
+	GameState.End : GameState.PostEnd
 }
 # the state determines what is visible and active etc.
 var currentState := GameState.Menu
-# counter for inside state stuff
-var currentStateUtilCounter := 0
 
 func advance_state():
 	currentState = nextState[currentState]
@@ -88,8 +101,56 @@ var dialogs := [
 		[2, "Oh! I know I’m gonna add some..."]
 	],
 	[
-		
-	]
+		[2, "And maybe some..."]
+	],
+	[
+		[1, "Well, I suck at hard games personally..." ],
+		[2, "Oh, but people really like them. You know, the challenge is what makes it fun." ], 
+		[2, "Overcoming it. It’s like real life!" ],
+		[1, "..." ],
+		[1, "right." ],
+		[1, "Hey, how nice, it’s snowing outside. Do you wanna go out?" ],
+		[2, "Nah. I feel like I’m really close now..." ]
+	],
+	[
+		[1, "Hey the controls look a bit stiff?"],
+		[2, "Yea, it makes it harder. But maybe I could make them a little worse?"],
+		[1, "Okey... You sure?"],
+		[2, "If it’s a little harder, it will be okay. Everything will be okay."],
+		[1, "huh?"]
+	],
+	[
+		[2, "And maybe some..."]
+	],
+	[
+		[1, "Hey I think that’s pretty great. That seems like a satisfying challenge?" ],
+		[2, "No..." ],
+		[2, "Still not good enough." ],
+		[2, "Gotta make it harder. That will solve everything." ],
+		[1, "Solve? How do you mean?" ],
+		[2, "..." ],
+		[1, "..." ]
+	],
+	[
+		[1, "Great job! I think that’s really impressive... Hey..."]
+	],
+	[
+		[ 1, "Are you okay?"],
+		[ 2, "I... "],
+		[ 2, "..."],
+		[ 2, "No..."],
+		[ 2, "I..."],
+		[ 2, "I think I need help..."],
+		[ 2, "I thought that maybe if I made this game and it was really good..."],
+		[ 2, "That... It would help... that I would be..."],
+		[ 2, "happy... but..."],
+		[ 1, "I think it’s brave to admit that. Better than acting bold, and pretending everything's fine..."],
+		[ 1, "We all need some help sometimes. How about we take a break now and go enjoy the snow?"],
+		[ 2, "..."],
+		[ 2, "Yeah. Yeah I think that would be nice."],
+		[ 2, "Thank you."],
+	],
+	
 ]
 
 func hide_dialog() -> void:
@@ -103,7 +164,7 @@ func show_dialog() -> void:
 	var data = dialogs[dialogIndex][lineIndex]
 	
 	# TODO: show / hide the thingies
-	
+	print("Showing ", dialogIndex, ' ', lineIndex)
 	$DialogBody/DialogWindow.text = data[1]
 	
 # -------------------------------------------------------------------------
@@ -126,14 +187,32 @@ func level_beaten() -> void:
 			advance_state()
 			applyState()
 		
-		# In later ones, increment counter and based on it do other stuff
-	
-func player_die() -> void:
-	pass
+		GameState.SecondLevelChoice:
+			show_dialog()
+			show_available_options()
+		
+		GameState.ThirdLevelChoice:
+			show_dialog()
+			show_available_options()
+		
+		GameState.Four:
+			$"/root/Level1".end_all()
+			
+			currentState = GameState.Won
+			applyState()
+
+func level_give_up() -> void:
+	advance_dialog()
+	currentState = GameState.End
+	applyState()
 
 func dialog_next() -> void:
 	# have to pick from the options to advance
-	if currentState == GameState.SecondLevelChoice and lineIndex+1 == len(dialogs[dialogIndex]):
+	if currentState == GameState.SecondLevelChoice:
+		return
+	
+	# have to pick from the options to advance
+	if currentState == GameState.ThirdLevelChoice:
 		return
 	
 	advance_dialog()
@@ -153,11 +232,91 @@ func dialog_finished() -> void:
 	advance_state()
 	applyState()
 
+# -------------------------------------------------------------------------
+
+var used_options := []
+
+# keep track of used / available
+func show_available_options() -> void:
+	
+	if currentState == GameState.SecondLevelChoice and len(used_options) >= 3:
+		used_options.clear()
+		currentState = GameState.AllSecondLevelBeaten
+		applyState()
+		return
+	
+	if currentState == GameState.ThirdLevelChoice and len(used_options) >= 3:
+		used_options.clear()
+		currentState = GameState.AllThirdBeaten
+		applyState()
+		return
+	
+	if currentState == GameState.ThirdLevelChoice and len(used_options) == 1:
+		currentState = GameState.MidThird
+		applyState()
+		used_options.append("buffer")
+		return
+	
+	if currentState == GameState.SecondLevelChoice:
+		$Spikes.visible = !used_options.has("spikes")
+		$Pits.visible = !used_options.has("pits")
+		$Platforms.visible = !used_options.has("platforms")
+	
+	if currentState == GameState.ThirdLevelChoice:
+		$Sawblade.visible = !used_options.has("sawblade")
+		$Pikes.visible = !used_options.has("pikes")
+
+func hide_options() -> void:
+	$Spikes.visible = false
+	$Pits.visible = false
+	$Platforms.visible = false
+	$Sawblade.visible = false
+	$Pikes.visible = false
+
+
+func option_spikes() -> void:
+	used_options.append("spikes")
+	$"/root/Level1".show_layer("spikes")	
+	hide_options()
+	hide_dialog()
+
+func option_pits() -> void:
+	used_options.append("pits")
+	$"/root/Level1".show_layer("pits")
+	hide_options()	
+	hide_dialog()
+
+func option_platforms() -> void:
+	used_options.append("platforms")
+	$"/root/Level1".show_layer("platforms")
+	hide_options()
+	hide_dialog()
+
+
+func option_sawblades() -> void:
+	used_options.append("sawblade")
+	$"/root/Level1".show_layer("sawblade")
+	hide_options()
+	hide_dialog()
+
+func option_pikes() -> void:
+	used_options.append("pikes")
+	$"/root/Level1".show_layer("pikes")
+	hide_options()
+	hide_dialog()
+
+# -------------------------------------------------------------------------
+
+
 func applyState() -> void:
+	
+	print("Applying ", currentState)
 	
 	match currentState:
 		GameState.Menu:
 			hide_dialog()
+			dialogIndex = 0
+			lineIndex = 0
 		
 		GameState.Introduction:
 			show_dialog()
@@ -169,9 +328,46 @@ func applyState() -> void:
 		
 		GameState.FirstLevelPlay:
 			hide_dialog()
+			
+		GameState.PreSecondChoice:
+			show_dialog()
 		
 		GameState.SecondLevelChoice:
+			show_available_options()
+		
+		GameState.AllSecondLevelBeaten:
+			hide_dialog()
+			advance_dialog()
+		
+		GameState.PreThirdLevelChoice:
 			show_dialog()
+		
+		GameState.ThirdLevelChoice:
+			show_available_options()
+		
+		GameState.AllThirdBeaten:
+			hide_dialog()
+			advance_dialog()
+		
+		GameState.MidThird:
+			lineIndex = 0
+			show_dialog()
+
+		GameState.PreFourth:
+			show_dialog()
+		
+		GameState.Four:
+			hide_dialog()
+		
+		GameState.Won:
+			show_dialog()
+		
+		GameState.End:
+			show_dialog()
+		
+		GameState.PostEnd:
+			hide_dialog()
+			# load thank you screen (with ending message) / menu
 		
 		_:
 			$DialogBody.visible = true
--- scripts/level.gd
+++ scripts/level.gd
@@ -1,4 +1,18 @@
 extends Node2D
 
 
+# has the player, does everything gameplay-wise
+# know when to use input and when to ignore etc.
+
+
+
 # Will toggle the separate layers 
+func show_layer(layer) -> void:
+	print("Showing ", layer)
+	
+	# reset the level
+
+# TODO: unload this level and load blank again
+func end_all() -> void:
+	
+	pass
